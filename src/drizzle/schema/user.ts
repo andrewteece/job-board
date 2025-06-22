e062c1990085d242f6e0e816c61b2b64ad4fd182
andrewteece <andrewteece@gmail.com>
2025-06-22 09:29:53 -0500

    userResume code and update schema.ts

--- src/drizzle/schema/user.ts
+++ src/drizzle/schema/user.ts
@@ -1,5 +1,5 @@
 import { pgTable, varchar } from 'drizzle-orm/pg-core';
-import { createdAt, updatedAt } from '../schemaHelpers';
+import { createdAt, updatedAt } from '..//schemaHelpers';
 import { relations } from 'drizzle-orm';
 import { UserResumeTable } from './userResume';
 import { UserNotificationSettingsTable } from './userNotificationSettings';
