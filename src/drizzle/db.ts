ccca5a8fc754799376d203da264b07eebbd55a9b
andrewteece <andrewteece@gmail.com>
2025-06-23 14:54:35 -0500

    setup new docker, update server and db.ts and migrate drizzle

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,11 +1,17 @@
+// import { env } from '@/data/env/server';
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import * as schema from '@/drizzle/schema';
+// import { Pool } from 'pg';
+
+// // export const db = drizzle(env.DATABASE_URL, { schema });
+// const pool = new Pool({
+//   connectionString: env.DATABASE_URL,
+// });
+
+// export const db = drizzle(pool);
+
 import { env } from '@/data/env/server';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@/drizzle/schema';
-import { Pool } from 'pg';
 
-// export const db = drizzle(env.DATABASE_URL, { schema });
-const pool = new Pool({
-  connectionString: env.DATABASE_URL,
-});
-
-export const db = drizzle(pool);
+export const db = drizzle(env.DATABASE_URL, { schema });
