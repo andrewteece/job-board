21a490aee5e1bc7534836661e0fe5b63bfdcbadd
andrewteece <andrewteece@gmail.com>
2025-06-24 16:45:22 -0500

    set sidebar for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,28 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
 
 export default function HomePage() {
   return (
     <SidebarProvider className='overflow-y-hidden'>
-      <Sidebar collapsible='icon' className='overflow-hidden'>
-        <SidebarHeader className='flex-row'>
-          <SidebarTrigger />
-          <span className='text-xl text-nowrap'>WebDev Jobs</span>
-        </SidebarHeader>
-        <SidebarContent>test</SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton>123</SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
-      </Sidebar>
-      <main className='flex-1'>abcdef</main>
+      <AppSidebarClient>
+        <Sidebar collapsible='icon' className='overflow-hidden'>
+          <SidebarHeader className='flex-row'>
+            <SidebarTrigger />
+            <span className='text-xl text-nowrap'>WebDev Jobs</span>
+          </SidebarHeader>
+          <SidebarContent>test</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>123</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className='flex-1'>abcdef</main>
+      </AppSidebarClient>
     </SidebarProvider>
   );
 }
