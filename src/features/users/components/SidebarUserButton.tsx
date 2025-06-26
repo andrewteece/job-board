c0e52470c923ca8df7474bfdbe3b51d3eb22d95a
andrewteece <andrewteece@gmail.com>
2025-06-26 14:31:53 -0500

    SidebarUserButton and getCurrentAuth

---
+++ src/features/users/components/SidebarUserButton.tsx
@@ -0,0 +1,28 @@
+import { SidebarMenuButton } from '@/components/ui/sidebar';
+import { SignOutButton } from '@clerk/nextjs';
+import { LogOutIcon } from 'lucide-react';
+import { Suspense } from 'react';
+import { getCurrentUser } from '@/services/clerk/lib/getCurrentAuth';
+
+export function SidebarUserButton() {
+  return (
+    <Suspense>
+      <SidebarUserSuspense />
+    </Suspense>
+  );
+}
+
+async function SidebarUserSuspense() {
+  const { user } = await getCurrentUser({ allData: true });
+
+  if (user === null) {
+    return (
+      <SignOutButton>
+        <SidebarMenuButton>
+          <LogOutIcon />
+          <span>Log Out</span>
+        </SidebarMenuButton>
+      </SignOutButton>
+    );
+  }
+}
