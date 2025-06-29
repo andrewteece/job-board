a5e054e36419aaa90f03ba368eb5b3b30e387a85
andrewteece <andrewteece@gmail.com>
2025-06-29 16:49:22 -0500

    install svix and update client and clerk for inngest

--- src/services/inngest/client.ts
+++ src/services/inngest/client.ts
@@ -1,4 +1,21 @@
-import { Inngest } from 'inngest';
+import { EventSchemas, Inngest } from 'inngest';
+
+type ClerkWebhookData<T> = {
+  data: {
+    data: T;
+    raw: string;
+    headers: Record<string, string>;
+  };
+};
+
+type Events = {
+  'clerk/user.created': ClerkWebhookData<UserJSON>;
+  'clerk/user.updated': ClerkWebhookData<UserJSON>;
+  'clerk/user.deleted': ClerkWebhookData<DeletedObjectJSON>;
+};
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'job-board' });
+export const inngest = new Inngest({
+  id: 'job-board',
+  schemas: new EventSchemas().fromRecord<Events>(),
+});
